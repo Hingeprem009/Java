784bda8d53733a76bae3d657f84e063bd095cee1
Hingeprem009 <hingeprem6447@gmail.com>
2026-06-15 10:42:40 +0530

    Conditional statements folder added (if)

---
+++ ConditionalStatements/ifcondition.java
@@ -0,0 +1,16 @@
+package ConditionalStatements;
+
+class IfCondition{
+    public static void main(String[] args)
+    {
+        int age = 18;
+        if(age < 18)
+        {
+            System.out.println("You can't drive");
+        }
+        else
+        {
+            System.out.println("You can drive");
+        }
+    }
+}
