989cc5372377d2371bcddba9cb29e64c19ecf20c
Hingeprem009 <hingeprem6447@gmail.com>
2026-06-16 17:05:29 +0530

    Added Loops file

---
+++ Loops/DoWhile.java
@@ -0,0 +1,13 @@
+public class DoWhile
+{
+    public static void main(String[] args)
+    {
+        int i = 5;
+        do
+        {
+            System.out.println("Prem Hinge " + i);
+            i++;
+        }
+        while(i<=4); //Run's atleast one's.
+    }
+}
---
+++ Loops/ForLoop.java
@@ -0,0 +1,16 @@
+//Loops are use to repeat the things 
+//Types of loops are 
+//1 For Loop 
+//2 While Loop
+//3 Do while Loop (execute atleast one)
+public class ForLoop
+{
+    public static void main(String[] args)
+    {
+        int n = 10;
+        for(int i=0;i<=n;i++)
+        {
+            System.out.println("Hello " + i);
+        }
+    }
+}
