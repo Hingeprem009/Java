474523ed76276d7ceba0003d395ea1834d5c6c1a
Hingeprem009 <hingeprem6447@gmail.com>
2026-06-28 14:44:32 +0530

    Learned about down and up casting

---
+++ downUpCasting.java
@@ -0,0 +1,26 @@
+class A
+{
+    public void show1()
+    {
+        System.out.println("in A show");
+    }
+}
+class B extends A
+{
+    public void show2()
+    {
+        System.out.println("in B show");
+    }
+}
+
+public class downUpCasting
+{
+    public static void main(String[] args)
+    {
+        A obj = new B(); // Upcasting
+        obj.show1(); 
+
+        B obj1 = (B) obj;   //Downcasting
+        obj1.show2();
+    }
+}
