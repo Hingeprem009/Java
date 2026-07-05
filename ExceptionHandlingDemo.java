e4238565cd3936082d1aa47516e6578ad5471448
Hingeprem009 <hingeprem6447@gmail.com>
2026-07-05 13:07:18 +0530

    Added Multiple catch blocks

--- ExceptionHandlingDemo.java
+++ ExceptionHandlingDemo.java
@@ -2,18 +2,32 @@ public class ExceptionHandlingDemo
 {
     public static void main(String[] args)
     {
-        int i = 0;
+        int i = 2;
         int j = 0;
+
+        int[] nums =  new int[5];
+        String str = "prem";
         
         try
         {
             j = 18 / i;
+            System.out.println(str.length());
+            System.out.println(nums[1]);
+            System.out.println(nums[5]);
         }
-        catch (Exception e) // Executes when an exception occurs 
+        catch (ArithmeticException e) // Executes when an arithmetic exception occurs
         {
-            System.out.println("Something went wrong...");
+            System.out.println("Cannot divide by zero");
         }
-
-        System.out.println("Bye " + j);
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Please enter a valid index");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Something went wrong: " + e);
+        }
+        System.out.println(j);
+        System.out.println("Bye");
     }
 }
