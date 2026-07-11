0e94887936788b809b50acd017c30b194c202ee6
Hingeprem009 <hingeprem6447@gmail.com>
2026-07-11 09:56:47 +0530

    Comparator sorting example

---
+++ Sorting/ComparatorSortingDemo.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class ComparatorSortingDemo{
+    public static void main(String[] args)
+    {
+        Comparator<Integer> com = new Comparator<Integer>()
+        {
+            public int compare(Integer i, Integer j)
+            {
+                if(i%10 > j%10) //sorting element by last digit
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        };
+
+        List<Integer> nums = new ArrayList<>();
+        nums.add(43);
+        nums.add(31);
+        nums.add(72);
+        nums.add(29);
+
+        Collections.sort(nums, com);
+        System.out.println(nums);
+    }
+}
