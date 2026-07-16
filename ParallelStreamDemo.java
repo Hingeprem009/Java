bdba1666141f0ca8ed987f15ab5142799127b577
Hingeprem009 <hingeprem6447@gmail.com>
2026-07-16 11:43:27 +0530

    Updated the name of the file

--- ParallelStreamDemo.java
+++ ParallelStreamDemo.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-class test {
+class ParallelStreamDemo {
     public static void main(String[] args)
     {
         int size = 10000;
